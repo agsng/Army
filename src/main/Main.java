2ab136205ed117f7a4c889df4aae0163d6aef755
Amrit Gopal <amritgg977@gmail.com>
2020-02-04 17:09:56 +0530

    Update Main.java
    Tuesday commits

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,31 +1,23 @@
 package main;
 
+import java.util.*;
 
-import java.util.Scanner;
+import static main.army.getArmyTypeNames;
+import static main.calculate.calccc;
 
-import static main.falicornia_input.falicorniainput;
-import  static main.army.getEachArmyCount;
+import static main.army.getArmyCount;
 
-    public class Main {
+
+public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("How many Army Types are there??");
-        int n = 4;//sc.nextInt();
-        falicorniainput();     // input the Falicornia army count
+        ArrayList<Integer> LengaburuArmyCount = new ArrayList<>(Arrays.asList(100,50,10,5,8));
+        int n = LengaburuArmyCount.size();
 
-//        int [] LengaburuArmyCount = new int[n];
-//        for(int i=0;i<n;i++)
-//            LengaburuArmyCount[i]=sc.nextInt();
-           /* here we know the lengaburuarmy count so initializing the array*/
-        int [] LengaburuArmyCount = new int[]{100,50,10,5};
-        int [] falicorniaArmyCount = new int[n];
+        ArrayList<Integer> falicorniaArmyCount =  getArmyCount(n);
+        ArrayList<String> armyTypeNames = getArmyTypeNames();
 
-        for(int j=0;j<4;j++)
-        falicorniaArmyCount[j] = getEachArmyCount(j,n);
-//        for(int j=0;j<4;j++)
-//            System.out.print(falicorniaArmyCount[j]+" ");
-
+        calccc(LengaburuArmyCount,falicorniaArmyCount,armyTypeNames,n);
 
     }
 }
